c8d20237e274cb18c600d2af1d5c9d5dc7b10d95
Jade Barben <barbenjade@gmail.com>
2019-05-28 19:21:58 -0600

    retry three

---
+++ sequence5.php
@@ -0,0 +1,52 @@
+<?php
+
+$sequence = array(40, 50, 60, 10, 20, 30);
+function almostIncreasingSequence($sequence) {
+    $booly = true;
+    $endLoop = (count($sequence) - 1);
+    $arrSpliced = [];
+    $arrLimitCount = count($arrSpliced);
+    $arrUnique = array_unique($sequence);
+    
+    for($i = 0; $i < $endLoop; $i++){
+        $iPlusOne = $sequence[$i + 1];
+        $iMinusOne = $sequence[$i - 1];
+            if ($sequence[$iPlusOne] < $sequence[$i]) {
+                if ($i == 0){
+                    $arrSpliced[] = array_slice($sequence, $i, 1);
+                    echo $i . "yessssss";
+                } else if ($iPlusOne > $iMinusOne){
+                    echo "one";
+                    echo $iPlusOne;
+                    echo $iMinusOne;
+                    $arrSpliced[] = array_slice($sequence, $i, 1);
+                } else if ($iPlusOne < $iMinusOne){
+                    echo "two";
+                    $arrSpliced[] = array_slice($sequence, $iPlusOne, 1);
+                } else {
+                    $arrSpliced[] = array_slice($sequence, $iPlusOne, 1);
+                    $endLoop = count($sequence) - 1;
+                    print_r($sequence);
+                    echo "Sequence rendered 1 <br>";
+                    print_r($arrSpliced);
+                    print(count($arrSpliced));
+                    echo "Array Spliced rendered 1 <br>";
+                }
+                } else if (array_unique($sequence) != $sequence){
+                $arrSpliced[] = array_slice($sequence, $iPlusOne, 1);
+            }
+            
+            if (count($arrSpliced) < 2) {
+                $booly = true;
+            } else {
+                $booly = false;
+             } 
+        }
+    echo "<br><br>";
+    print_r($sequence);
+    var_dump($booly);
+}
+almostIncreasingSequence($sequence);
+
+
+?>
